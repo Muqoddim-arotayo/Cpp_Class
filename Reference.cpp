9dd5b2c896fc8ffe1a02867ad68b766921a3b68c
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-08 15:18:44 +0100

    create a new file class file on C++ Classes

--- Reference.cpp
+++ Reference.cpp
@@ -2,7 +2,22 @@
 #include <string>
 using namespace std;
 
+
+void increment (int& value){
+    (value)++;
+    cout << "\n" << value;
+}
+
 int main(){
     // A reference variable is an alias for an existing variable. It is created using the & operator:
+    //  Reference are just pointers in disguise
+
+    int a = 5;
+    int& ref_a = a ; // an alias
+    ref_a = 2;
+    cout << "\n" << ref_a;
     cout << "Reference";
+
+    int b = 10;
+    increment(b);
 }
