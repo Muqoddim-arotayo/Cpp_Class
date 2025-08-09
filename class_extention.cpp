4db946f671713ec0746fad6e763d9b78c22ea9fa
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-09 21:57:20 +0100

    added a new "static" class file

--- class_extention.cpp
+++ class_extention.cpp
@@ -4,12 +4,30 @@ using namespace std;
 
 class Log {
     public:
-    void SetLevel (int level){
-        
-    }
-    void Warn (const char* message){
-        
-    }
+        const int LogLevelError = 0;
+        const int LogLevelWarning = 1;
+        const int LogLevelInfo = 2;
+    private:
+        int m_LogLevel =  LogLevelWarning ;
+    public:
+        void SetLevel (int level){
+            m_LogLevel = level;
+        }
+        void Warn (const char* message){
+            if (m_LogLevel >= LogLevelWarning){
+                cout << "[WARNING]" << message << endl ;
+            }
+        }
+        void Error (const char* message){
+            if (m_LogLevel >= LogLevelError){
+                cout << "[ERROR]" << message << endl ;
+            }
+        }
+        void Info (const char* message){
+            if (m_LogLevel >= LogLevelInfo){
+                cout << "[INFO]" << message << endl ;
+            }
+        }
 
 };
 
@@ -17,6 +35,12 @@ int main (){
     // CREATING A LOG CLASS
 
     Log log;
-    log.SetLevel(LogLevelWarning);
+    log.SetLevel(log.LogLevelWarning);
     log.Warn("Hello !");
+    log.Error("Hello !");
+    log.Info("Hello !");
+    log.SetLevel(log.LogLevelError);
+    log.Warn("Hello !");
+    log.Error("Hello !");
+    log.Info("Hello !");
 }
