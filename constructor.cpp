aeccf8f78957f37e2a129ecd9b43d2901a0f6a35
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-11 18:57:01 +0100

    added a new encapsulation class file

--- constructor.cpp
+++ constructor.cpp
@@ -2,8 +2,39 @@
 #include <string>
 using namespace std;
 
+class Entity {
+    public:
+    float X,Y;
+
+    Entity(){
+        X = 0.0f;
+        Y = 0.0f;
+    }
+
+    Entity (float x, float y){
+        X = x;
+        Y = y;
+    }
+
+    void Print (){
+        cout << X << " , " << Y << endl;
+    }
+};
+
+class Log {
+public:
+    static void write (){
+
+    }
+};
+
 int main (){
     // CONSTRUCTOR
     // A constructor is a special method that is automatically called when an object of a class is created.
-    cout << "constructor";
+    // It is a type of method that runs every time we instantiate an object
+
+    cout << "constructor" << endl;
+    Entity e(10.0, 5.0);
+    cout << e.X << endl;
+    e.Print();
 }
