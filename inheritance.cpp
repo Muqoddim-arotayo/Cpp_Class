305a8c7675f6ddac4d1851400a4c202fd51c365e
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-13 15:35:55 +0100

    added a new polymorphism class folder

--- inheritance.cpp
+++ inheritance.cpp
@@ -19,6 +19,20 @@ class car : public vehicle {
         }
 };
 
+class rocket {
+    public:
+        void fly () {
+            cout << "Flying..." << endl;
+        }   
+};
+
+// Multi-level Inheritance: A class can also be derived from one class, which is already derived from another class.
+
+// Multiple Inheritance: A class can also be derived from more than one base class, using a comma-separated list
+class flying_car : public car, public rocket {
+       
+};
+
 int main (){
     // Inheritance allows one class to reuse attributes and methods from another class. It helps you write cleaner, more efficient code by avoiding duplication.
     // 1. derived class (child) - the class that inherits from another class
@@ -29,5 +43,13 @@ int main (){
     my_car.move();
     my_car.detail("lexus","350","2025");
     my_car.park();
+
+    flying_car my_flying_car;
+    my_flying_car.fly();
+    my_flying_car.detail("tesla","flight","2025");
+
+    rocket apollo;
+    apollo.fly();
+
 
 }
