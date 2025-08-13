ac26eee1424fa6f2732c05480991c43a2256b98e
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-13 11:45:42 +0100

    added a new friend keyword class file

--- Access_specifiers.cpp
+++ Access_specifiers.cpp
@@ -2,6 +2,25 @@
 #include <string>
 using namespace std;
 
+// Access specifiers controls how the members (attributes and methods) of a class can be accessed
+
+class my_class {
+    // PUBLIC - which means that they can be accessed and modified from outside the code
+    public:
+    int public_int = 7;
+    
+    // PRIVATE - members cannot be accessed (or viewed) from outside the class
+    private:
+    int private_int = 10;
+
+    // PROTECTED - members cannot be accessed from outside the class, however, they can be accessed in inherited classes. You will learn more about
+    protected:
+    int protected_int = 15;
+};
+
 int main (){
     cout << "Access specifier" << endl;
+    my_class my_class_test;
+    cout << my_class_test.public_int ;
+
 }
--- encapsulation.cpp
+++ encapsulation.cpp
@@ -2,6 +2,26 @@
 #include <string>
 using namespace std;
 
+class my_password {
+    private:
+    int pass_code;
+
+    public:
+    void setPassword(int code){
+        pass_code = code;
+    }
+    int getPassword(){
+        return pass_code;
+    }
+};
+
 int main(){
+    // Encapsulation is to make sure that "sensitive" data is hidden from users.
+    // If you want others to read or modify the value of a private member, you can provide public get and set methods.
+    
     cout << "encapsulation";
+    my_password user_pass_word;
+    user_pass_word.setPassword(9087);
+    cout << user_pass_word.getPassword() << endl;
+    return 0;
 }
