6a14a8f6cacff70567492eca5cede3c8d2d57e86
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-15 20:56:42 +0100

    added a new visibility class file

---
+++ Interfaces_in_c++.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    cout << "Interfaces in C++" ;
+    // An interface is a class that mostly consists of unimplemented methods acting as a template of sort
+}
--- Virtual_function.cpp
+++ Virtual_function.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Entity {
     public:
-        string get_name (){
+        virtual string get_name(){
             return "Entity";
         }
 };
@@ -13,12 +13,35 @@ class player : public Entity {
     private:
         string m_name;
     public:
-        player(const string& name)
-        :m_name(name){}
-    string get_name(){return m_name;}
+        player(const string& name){
+            m_name = name;
+        }
+
+        string get_name(){
+            return m_name;
+        }
 };
+
+void print_name(Entity* entity){
+    cout << entity->get_name();
+}
 
 int main (){
     // Virtual functions allows us to override functions in sub classes
-    cout << "Virtual function";
+    cout << "Virtual function" << endl;
+
+    Entity* e = new Entity;
+    print_name(e);
+
+    player* p = new player("eniola");
+    print_name(p);
+
+
+
+    // Entity* e = new Entity();
+    // cout << e->get_name() << endl ;
+
+    // player* p = new player("eniola");
+    // cout << p->get_name() << endl;
+
 }
