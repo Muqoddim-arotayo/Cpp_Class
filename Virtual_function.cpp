11c25f02290325697c63e7233515912ddbe70bf5
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-15 19:55:08 +0100

    added a new class file on the interfaces in c++, pure virtual function.

--- Virtual_function.cpp
+++ Virtual_function.cpp
@@ -2,6 +2,23 @@
 #include <string>
 using namespace std;
 
+class Entity {
+    public:
+        string get_name (){
+            return "Entity";
+        }
+};
+
+class player : public Entity {
+    private:
+        string m_name;
+    public:
+        player(const string& name)
+        :m_name(name){}
+    string get_name(){return m_name;}
+};
+
 int main (){
+    // Virtual functions allows us to override functions in sub classes
     cout << "Virtual function";
 }
