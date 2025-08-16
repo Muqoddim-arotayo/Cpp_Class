635210acf80304626431770bc48a325045b85bbd
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-16 20:18:03 +0100

    added extra feature to the destructor class file

--- visibility.cpp
+++ visibility.cpp
@@ -2,6 +2,38 @@
 #include <string>
 using namespace std;
 
+class Entity {
+private:
+    int X,Y;
+protected:
+    int A,B;
+public:
+    Entity(){
+
+    }
+};
+
+class player : public Entity{
+    public:
+        player(){
+            // X = 3;
+            A = 5;
+
+        }
+};
+
 int main (){
     cout << "visibility" << endl ; 
+    // Visiblity doesnt have any effect on how your program runs and no effect on program perfomance
+    // 3 basic visibility modifiers in C++
+    // - Public
+    // - Private
+    // - Protected
+    // Default visibility in C++ is private 
+
+    // Making everything public is a bad idea
+
+    player p;
+    // p.A = 7;
+
 }
