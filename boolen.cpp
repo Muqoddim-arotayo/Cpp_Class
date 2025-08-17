c4fee9cf2dff3e0fff59c72ee01e597b8a85a96f
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-08-17 20:12:37 +0100

    added some extra properties to my boolean class file

--- boolen.cpp
+++ boolen.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    bool is_coding_fun = true;
+    bool is_coding_fun = true; 
     bool is_fish_tasty = false;
     cout << is_coding_fun ;
     cout << is_fish_tasty ;
